144f18dac8196f65f04032f2529bbb526873e470
Chaya Deaver <chayadeaver@gmail.com>
2020-01-27 20:52:38 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,8 +9,7 @@ class ArtistsController < ApplicationController
     end
 
     def create
-        @artist = Artist.new(artist_params(:name, :bio))
-        @artist.save
+        @artist = Artist.create(artist_params(:name, :bio))
         redirect_to artist_path(@artist)
     end
 
