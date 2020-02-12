276fedb043513706957afc3a09f3a94ab63440f1
jaredjames2020 <jaredjames2020@gmail.com>
2020-02-11 19:26:43 -0500

    genres form and show views

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,23 @@
 class GenresController < ApplicationController
+
+  def new
+    @genre = Genre.new
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  private
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
+
 end
