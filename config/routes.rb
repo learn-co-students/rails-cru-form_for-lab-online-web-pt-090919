ce19720d4e283e8530def5397b12df9e2016addc
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-16 06:46:03 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,32 @@
 Rails.application.routes.draw do
-  resources :artists, :songs, :genres, only: [:create, :edit, :update, :show]
-  get '/song/create', to: 'song#create'
+  resources :songs
+  resources :genres
+  resources :artists
   
-  get '/song/:id/edit', to: 'songs#edit'
-
-  patch '/songs/:id', to: 'songs#update'
-
-  get '/songs/:id/show', to: 'songs#show'
-
-  get '/genres/create', to: 'genres#create'
+  # resources :artists, :songs, :genres, only: [:create, :edit, :update, :show]
+  # get '/song/create', to: 'song#create'
   
-  get '/genres/:id/edit', to: 'genres#edit'
+  # get '/song/:id/edit', to: 'songs#edit'
 
-  patch '/genres/:id', to: 'genres#update'
+  # patch '/songs/:id', to: 'songs#update'
 
-  get '/genres/:id', to: 'genres#show'
+  # get '/songs/:id/show', to: 'songs#show'
 
-  get '/artists/create', to: 'artists#create'
-
-  get '/artists/:id/update', to: 'artists#edit'
+  # get '/genres/create', to: 'genres#create'
   
-  patch '/artists/:id', to: 'artists#update'
+  # get '/genres/:id/edit', to: 'genres#edit'
 
-  get '/artists/show', to: 'artists#show'
+  # patch '/genres/:id', to: 'genres#update'
+
+  # get '/genres/:id', to: 'genres#show'
+
+  # get '/artists/create', to: 'artists#create'
+
+  # get '/artists/:id/update', to: 'artists#edit'
+  
+  # patch '/artists/:id', to: 'artists#update'
+
+  # get '/artists/show', to: 'artists#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
